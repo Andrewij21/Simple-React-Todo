16791482253dd5ec2d28987e3cbfcb593842cd4f
Andrewij21 <andrewijaya988@gmail.com>
2023-08-01 21:00:59 +0700

    feat: can create data to firebase

--- src/Pages/NewMeetup.jsx
+++ src/Pages/NewMeetup.jsx
@@ -1,9 +1,25 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useNavigate } from "react-router-dom";
 function NewMeetup() {
+  const navigate = useNavigate();
+  async function addMeetupHandler(meetup) {
+    await fetch(
+      "https://react-meetups-ddc68-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetup),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate("/");
+    });
+  }
   return (
     <section>
       <h1>Add Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
