4a0cca42cddf3071800f94535e71c5eb984f0193
Andrewij21 <andrewijaya988@gmail.com>
2023-08-01 21:58:28 +0700

    feat: favorite page have content

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -1,5 +1,25 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavoriteContext from "../store/FavoriteContext";
+import PropTypes from "prop-types";
+
+Favorites.propTypes = {
+  favoritectx: PropTypes.object,
+};
+
 function Favorites() {
-  return <div>Favorites</div>;
+  const favoritectx = useContext(FavoriteContext);
+  let content;
+  if (favoritectx.totalFavorites === 0)
+    content = <p>You have no favorites, start adding one?</p>;
+  else content = <MeetupList meetups={favoritectx.favorites} />;
+
+  return (
+    <section>
+      <h1>Favorites Meetups</h1>
+      {content}
+    </section>
+  );
 }
 
 export default Favorites;
