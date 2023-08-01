8090384af3bc9db6ae3bf1de1e961cf1fae778af
Andrewij21 <andrewijaya988@gmail.com>
2023-08-01 21:45:10 +0700

    feat: add favorite context component

---
+++ src/store/FavoriteContext.jsx
@@ -0,0 +1,48 @@
+import { createContext, useState } from "react";
+import PropTypes from "prop-types";
+
+FovoriteContextProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+const FavoriteContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+  addMeetup: () => {},
+  removeMeetup: () => {},
+  itemIsFavorite: () => {},
+});
+
+export function FovoriteContextProvider(props) {
+  const [favorites, setFavorites] = useState([]);
+
+  function addMeetupHandler(meetup) {
+    setFavorites((prev) => {
+      return prev.concat(meetup);
+    });
+  }
+  function removeMeetupHandler(meetupId) {
+    setFavorites((prev) => {
+      return prev.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+  function itemIsFavoriteHandler(meetupId) {
+    return favorites.some((meetup) => meetup.id === meetupId);
+  }
+
+  const context = {
+    favorites,
+    totalFavorites: favorites.length,
+    addMeetup: addMeetupHandler,
+    removeMeetup: removeMeetupHandler,
+    itemIsFavorite: itemIsFavoriteHandler,
+  };
+  console.log(favorites);
+  return (
+    <FavoriteContext.Provider value={context}>
+      {props.children}
+    </FavoriteContext.Provider>
+  );
+}
+
+export default FavoriteContext;
