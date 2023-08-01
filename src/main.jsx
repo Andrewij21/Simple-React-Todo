c9338df8ef1a2b5f4767be6446046751e0312e74
Andrewij21 <andrewijaya988@gmail.com>
2023-08-01 21:44:38 +0700

    feat: add context provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { FovoriteContextProvider } from "./store/FavoriteContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <FovoriteContextProvider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </FovoriteContextProvider>
 );
