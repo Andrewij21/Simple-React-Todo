58fa02c9ffac47c33101fbbe2e63d5793946f79e
Andrewij21 <andrewijaya988@gmail.com>
2023-08-10 18:40:24 +0700

    refactor: change typo

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { FovoriteContextProvider } from "./store/FavoriteContext.jsx";
+import { FavoriteContextProvider } from "./store/FavoriteContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <FovoriteContextProvider>
+  <FavoriteContextProvider>
     <BrowserRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </BrowserRouter>
-  </FovoriteContextProvider>
+  </FavoriteContextProvider>
 );
